4b464b96f12de58180b2190ba802425e1930b23a
Enio Filipe <filipeenio1234@gmail.com>
2025-05-31 15:38:43 -0300

    first commit with structures

---
+++ SGEA/src/main/java/com/mycompany/sgea/SGEA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sgea;
+
+/**
+ *
+ * @author enio1
+ */
+public class SGEA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
